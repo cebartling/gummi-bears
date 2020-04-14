6f34312d3f7c1f71fd400f8c3aa5be81b93bec5f
Christopher Bartling <chris.bartling@gmail.com>
2020-04-13 19:24:11 -0500

    Configuring Rack::Cors, take 3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # global options responder -> makes sure OPTION request for CORS endpoints work
+  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
+
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
