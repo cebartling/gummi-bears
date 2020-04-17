eec503048c4b249c3987d674bc72b9a9c11b01b4
Christopher Bartling <chris.bartling@gmail.com>
2020-04-17 16:46:58 -0500

    Created a data seeding for user_stocks.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'seeds/users/users_seed'
 require 'seeds/stocks/stocks_seed'
+require 'seeds/user_stocks/user_stocks_seed'
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
@@ -13,4 +14,5 @@ require 'seeds/stocks/stocks_seed'
 if Rails.env.development?
   Seeds::Users::UsersSeed.execute
   Seeds::Stocks::StocksSeed.execute
+  Seeds::UserStocks::UserStocksSeed.execute
 end
