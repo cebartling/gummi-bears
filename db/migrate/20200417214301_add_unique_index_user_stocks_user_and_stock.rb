2e3acaf40be76165a4009983ced33e4dfb53f999
Christopher Bartling <chris.bartling@gmail.com>
2020-04-17 16:47:07 -0500

    Created a data seeding for user_stocks.

--- db/migrate/20200417214301_add_unique_index_user_stocks_user_and_stock.rb
+++ db/migrate/20200417214301_add_unique_index_user_stocks_user_and_stock.rb
@@ -3,3 +3,4 @@ class AddUniqueIndexUserStocksUserAndStock < ActiveRecord::Migration[6.0]
     add_index :user_stocks, [:user_id, :stock_id], unique: true, name: 'user_id_stock_id_unique_idx'
   end
 end
+
