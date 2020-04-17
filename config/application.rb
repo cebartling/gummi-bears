1c6a1ec8fe2a3ffaea2e0b73ef2e4e63bdbad670
Christopher Bartling <chris.bartling@gmail.com>
2020-04-16 23:28:30 -0500

    Fixing issue with Dotenv.

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module GummiBears
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
