25ed1adf891d567bc93c1f04dcaeacb87d4bdb08
Christopher Bartling <chris.bartling@gmail.com>
2020-04-18 22:23:02 -0500

    Updated GraphQL schema for authToken on User.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -31,5 +31,15 @@ module Types
     def user_by_username(username:)
       User.find_by_username(username)
     end
+
+    field :user_by_auth_token, UserType,
+          null: true,
+          description: 'Find a user by the auth token' do
+      argument :auth_token, String, required: true
+    end
+
+    def user_by_auth_token(auth_token:)
+      User.find_by(auth_token: auth_token)
+    end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,6 +4,7 @@ module Types
     field :first_name, String, null: false, description: "User's first or given name"
     field :last_name, String, null: false, description: "User's last or family name"
     field :username, String, null: false, description: "User's username for authentication purposes"
+    field :auth_token, String, null: true, description: "User's auth token for authentication purposes"
     field :stocks, [UserStockType], null: false, description: "Stocks associated with this user"
   end
 end
