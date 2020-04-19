80293e990ed4f2241e50fe4ce3a3d0f6dba3e0d2
Christopher Bartling <chris.bartling@gmail.com>
2020-04-18 22:17:44 -0500

    Token authentication is now working.

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,11 +1,13 @@
 class BaseController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
   before_action :authenticate
 
   private
 
   def authenticate
     authenticate_or_request_with_http_token do |token, _options|
-      User.find_by(token: token)
+      User.find_by(auth_token: token)
     end
   end
 
