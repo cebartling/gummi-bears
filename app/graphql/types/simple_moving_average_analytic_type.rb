b5e075658f0cdbf26bb6516fc85a2bfd286c27a4
Christopher Bartling <chris.bartling@gmail.com>
2020-05-16 00:02:28 -0500

    Wiring up the GraphQL schema for simple moving average analytic.

--- app/graphql/types/simple_moving_average_analytic_type.rb
+++ app/graphql/types/simple_moving_average_analytic_type.rb
@@ -5,5 +5,6 @@ module Types
     field :interval_type, Types::IntervalType, null: false
     field :series_type, Types::SeriesType, null: false
     field :time_period, Integer, null: false
+    field :simple_moving_average_entries, [Types::SimpleMovingAverageEntryType], null: false
   end
 end
--- app/graphql/types/simple_moving_average_entry_type.rb
+++ app/graphql/types/simple_moving_average_entry_type.rb
@@ -4,6 +4,5 @@ module Types
     field :simple_moving_average_analytic, Types::SimpleMovingAverageAnalyticType, null: false
     field :observation_value_in_cents, Integer, "Transaction price for the stock", null: false
     field :event_timestamp, GraphQL::Types::ISO8601DateTime, "Transaction timestamp", null: false
-
   end
 end
