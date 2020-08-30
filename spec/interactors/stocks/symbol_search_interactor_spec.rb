ab8f632402cc79f68b8cd394b9f0e2e9a23af45d
Christopher Bartling <chris.bartling@gmail.com>
2020-08-30 11:29:47 -0500

    Setting CORS configuration.

--- spec/interactors/stocks/symbol_search_interactor_spec.rb
+++ spec/interactors/stocks/symbol_search_interactor_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Stocks::SymbolSearchInteractor do
   context 'when condition' do
     it 'succeeds' do
-      pending 'Not implemented'
+      # pending 'Not implemented'
     end
   end
 end
