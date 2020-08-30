5bf488dd80e6b72598131e0512e4d3ea0b9ce65b
Christopher Bartling <chris.bartling@gmail.com>
2020-08-30 11:54:13 -0500

    Rails configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,14 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://gummi-bears-dev.web.app',
-            'http://localhost:5000',
-            'http://127.0.0.1:5000'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://gummi-bears-dev.web.app',
+#             'http://localhost:5000',
+#             'http://127.0.0.1:5000'
+#
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
