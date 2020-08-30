658aea00c9bcc0f7d7b40c2179657288ee561340
Christopher Bartling <chris.bartling@gmail.com>
2020-08-30 11:48:41 -0500

    Rails configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "gummi-bears-dev.web.app"
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
